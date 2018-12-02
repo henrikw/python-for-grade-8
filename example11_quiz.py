16ac976f68b5fc80b4e9ebb86cdb9b15d9b5d503
Henrik Warne <henrik.warne@trioptima.com>
2018-12-02 17:51:46 +0100

    For quiz add question number, stastics at end

--- example11_quiz.py
+++ example11_quiz.py
@@ -13,19 +13,28 @@ questions = [
      "b": "5",
      "c": "4",
      "correct": "c"},
-]        
-    
+]
+
+correct = []
+wrong = []
+question_number = 0
 for question in questions:
-    print question.get("question")
+    question_number = question_number + 1
+    print "%s: %s" % (question_number, question.get("question"))
     print
     for letter in ("a", "b", "c"):
         print "%s: %s" % (letter, question.get(letter))
     print
     answer = raw_input("Pick a, b or c: ")
     if answer == question.get("correct"):
+        correct.append(question_number)
         print "Correct"
     else:
+        wrong.append(question_number)
         print "Wrong"
     sleep(2)
     print
 
+print "Results: %s correct answers (%s), %s wrong answers (%s)" %\
+      (len(correct), correct, len(wrong), wrong)
+print
